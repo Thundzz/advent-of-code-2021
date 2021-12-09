e853b22debce51140eed409106bc5b10f9782bb9
Thundzz <yacinenew@gmail.com>
2021-12-09 23:28:09 +0100

    Add second part solution for day 9

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ My solutions to advent of code 2021 in :
 
 ## Hydrothermal vents from day 5:
 ![Hydrothermal vents](./img/vents.png)
+
+## Basins from day 9:
+![Basins](./img/basins.png)
--- py/day_09/solve.py
+++ py/day_09/solve.py
@@ -1,4 +1,5 @@
 import numpy as np
+import heapq
 
 def neighbours(i, j, n, m):
     full_list = [
@@ -13,7 +14,6 @@ def parse_input(filename):
     with open(filename) as file:
         ls = file.readlines()
     n, m = len(ls), len(ls[0].strip())
-    print(n, m)
     mat = np.zeros((n, m))
     for i, l in enumerate(ls):
         mat[i, :] = [int(c) for c in l.strip()]
@@ -21,20 +21,45 @@ def parse_input(filename):
 
 def low_point_risk_levels(mat, n, m):
     mask = np.zeros_like(mat)
+    low_points = []
     for i in range(n):
         for j in range(m):
             if all(mat[i, j] < mat[ii, jj] for ii, jj in neighbours(i, j, n, m)):
                 mask[i, j] = mat[i, j] + 1
+                low_points.append((i, j))
+    return np.sum(mask), low_points
 
-    return np.sum(mask)
+def explore_basin(mat, n, m, i, j):
+    queue = [(i, j)]
+    basin = []
+    visited = np.zeros_like(mat)
+    while queue:
+        ci, cj = queue.pop()
+        if not visited[ci, cj]:
+            visited[ci, cj] = 1
+            basin.append((ci, cj))
+            for ii, jj in neighbours(ci, cj, n, m):
+                if not mat[ii, jj] == 9 and not visited[ii, jj]:
+                    queue.append((ii, jj))
+    return len(basin), basin
+
+def find_largest_basins(mat, low_points, n, m):
+    basin_sizes = []
+    for i, j in low_points:
+        l, b = explore_basin(mat, n, m, i, j)
+        heapq.heappush(basin_sizes, l)
+    largest_basins =  heapq.nlargest(3, basin_sizes)
+    return largest_basins
 
 def main():
     mat, n, m = parse_input("input.txt")
-    import matplotlib.pyplot as plt
-    plt.imshow(mat)
-    plt.show()
-    c = low_point_risk_levels(mat, n, m)
+    # import matplotlib.pyplot as plt
+    # plt.imshow(mat)
+    # plt.show()
+    c, low_points = low_point_risk_levels(mat, n, m)
     print(c)
+    largest_basins = find_largest_basins(mat, low_points, n, m)
+    print(np.prod(largest_basins))
 
 if __name__ == '__main__':
     main()
