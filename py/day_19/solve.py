41879556f4522a08a027a98a16c9f0cfd633f56c
Thundzz <yacinenew@gmail.com>
2021-12-20 20:47:45 +0100

    Make some advancement on day 19

--- py/day_19/solve.py
+++ py/day_19/solve.py
@@ -1,19 +1,26 @@
-
-from scipy.spatial.transform import Rotation as R
 from itertools import product
-from functools import cache
+from functools import lru_cache
+from operator import itemgetter
+from collections import Counter
 import numpy as np
-@cache
-def all_rotations():
-    axes = ["x", "y", "z"]
-    angles = range(0,360, 90)
-    matrices = []
-    for axe, angle in product(axes, angles):
-        mat = np.round(R.from_euler(axe, angle, degrees=True).as_matrix())
-        print(mat)
-        matrices.append(mat)
-    return set([str(m) for m in matrices])
 
+def totuple(m):
+    if m.shape == (1, 3):
+        return tuple(np.array(m).reshape(-1))
+    return (
+        tuple(np.array(m[0]).reshape(-1)),
+        tuple(np.array(m[1]).reshape(-1)),
+        tuple(np.array(m[2]).reshape(-1))
+    )
+
+def all_rotations():
+    xrot = np.matrix([[1, 0, 0], [0, 0,-1],[0,1,0]])
+    yrot = np.matrix([[0, 0, 1], [0, 1, 0],[-1,0,0]])
+    matrices = set()
+    for p,q,r,s in product(range(4), repeat=4):
+        m = (xrot**p).dot(yrot**q).dot(xrot**r).dot(yrot**s)
+        matrices.add(totuple(m))
+    return list(map(np.matrix, matrices))
 
 def parse_raw_scanner(raw_scanner):
     beacons = []
@@ -32,13 +39,52 @@ def parse_input(filename):
         scanners[idx] = parse_raw_scanner(raw_scanner.strip())
     return scanners
 
+def try_find_match(sc1, sc2):
+    _, sc1 = sc1
+    _, sc2 = sc2
+    rotations = all_rotations()
+
+    IIII = 0
+    for rot in rotations:
+        # print(sc2)
+        n = len(sc1)
+        m = len(sc2)
+        # print(n, m)
+        rotsc2 = [totuple(rot.dot(coords)) for coords in sc2]
+        # print(rotsc2)
+        xs1 = list(map(itemgetter(0), sc1))
+        xs2 = list(map(itemgetter(0), rotsc2))
+        # print(xs2)
+        mat = np.zeros((n, m), dtype=int)
+        diffs = []
+        for i, j in product(range(n), range(m)):
+            mat[i, j] =  xs1[i] - xs2[j]
+            diffs.append(xs1[i] - xs2[j])
+        c = Counter(diffs)
+        if c.most_common(1)[0][1] >= 3:
+            print("hi :)")
+        # import matplotlib.pyplot as plt
+        # plt.imshow(mat)
+        # IIII +=1
+        # plt.savefig(str(IIII) + ".jpg")
+def diffprint(i, j, s1, s2):
+    print(i, j)
+    return s1[i]-s2[j]
+
+
+def solve(scanners):
+    sc1, sc2, *others = scanners.items()
+    try_find_match(sc1, sc2)
+
 def main():
     scanners = parse_input("input.txt")
+    solve(scanners)
     # print(scanners)
 
 if __name__ == '__main__':
     main()
-    matrices = all_rotations()
-    print(len(matrices))
+    # matrices = all_rotations()
+    # print(matrices)
+    # print(len(matrices))
 
 
