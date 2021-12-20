1884feba29a507f590c0f565f34a7d9206e36706
Thundzz <yacinenew@gmail.com>
2021-12-21 00:21:20 +0100

    Implement matching alg

--- py/day_19/solve.py
+++ py/day_19/solve.py
@@ -39,34 +39,34 @@ def parse_input(filename):
         scanners[idx] = parse_raw_scanner(raw_scanner.strip())
     return scanners
 
+def most_common_count(axis, coords1, coords2):
+    n = len(coords1)
+    m = len(coords2)
+    xs1 = list(map(itemgetter(axis), coords1))
+    xs2 = list(map(itemgetter(axis), coords2))
+    mat = np.zeros((n, m), dtype=int)
+    diffs = []
+    for i, j in product(range(n), range(m)):
+        diff = xs1[i] - xs2[j]
+        mat[i, j] =  diff
+        diffs.append(diff)
+
+    c = Counter(diffs)
+    return c.most_common(1)[0][1]
+
 def try_find_match(sc1, sc2):
     _, sc1 = sc1
     _, sc2 = sc2
     rotations = all_rotations()
-
-    IIII = 0
     for rot in rotations:
-        # print(sc2)
-        n = len(sc1)
-        m = len(sc2)
-        # print(n, m)
-        rotsc2 = [totuple(rot.dot(coords)) for coords in sc2]
-        # print(rotsc2)
-        xs1 = list(map(itemgetter(0), sc1))
-        xs2 = list(map(itemgetter(0), rotsc2))
-        # print(xs2)
-        mat = np.zeros((n, m), dtype=int)
-        diffs = []
-        for i, j in product(range(n), range(m)):
-            mat[i, j] =  xs1[i] - xs2[j]
-            diffs.append(xs1[i] - xs2[j])
-        c = Counter(diffs)
-        if c.most_common(1)[0][1] >= 3:
-            print("hi :)")
-        # import matplotlib.pyplot as plt
-        # plt.imshow(mat)
-        # IIII +=1
-        # plt.savefig(str(IIII) + ".jpg")
+
+        rotsc2 = [totuple(rot.dot(coord)) for coord in sc2]
+        mcc_x = most_common_count(0, sc1, rotsc2)
+        mcc_y = most_common_count(1, sc1, rotsc2)
+        mcc_z = most_common_count(2, sc1, rotsc2)
+        if mcc_x >= 12 and mcc_y >= 12 and mcc_z >= 12:
+            print(rot)
+
 def diffprint(i, j, s1, s2):
     print(i, j)
     return s1[i]-s2[j]
@@ -77,14 +77,11 @@ def solve(scanners):
     try_find_match(sc1, sc2)
 
 def main():
-    scanners = parse_input("input.txt")
+    scanners = parse_input("test.txt")
     solve(scanners)
-    # print(scanners)
 
 if __name__ == '__main__':
     main()
     # matrices = all_rotations()
-    # print(matrices)
-    # print(len(matrices))
 
 
