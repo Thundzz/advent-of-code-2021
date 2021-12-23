02d406dee51e82e5e221ec7255fb57b0043428d1
Thundzz <yacinenew@gmail.com>
2021-12-23 23:47:31 +0100

    Create project structure for day 23

---
+++ py/day_23/solve.py
@@ -0,0 +1,22 @@
+
+
+
+"""
+#0123456789a#
+#hhhhhhhhhhh#
+##1A#.#.#.###
+  2A#.#.#.#
+  #########
+
+let's denote hallway places h0 to ha
+and rooms A1, A2, B1, B2, C1, C2, D1, D2
+"""
+
+def main():
+    _ = parse_input("input.txt")
+
+if __name__ == '__main__':
+    main()
+
+
+
