e77f7f5913d97e21a04083de9ebed9b33d4ca2cc
Thundzz <yacinenew@gmail.com>
2021-12-27 00:12:43 +0100

    Start exploring day 23 problem

---
+++ py/day_23/helper.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+import heapq
+
+"""
+#0123456789a#
+#hhhhhhhhhhh#
+##1A#.#.#.###
+  2A#.#.#.#
+  #########
+
+let's denote hallway places h0 to ha
+and rooms A1, A2, B1, B2, C1, C2, D1, D2
+"""
+
+graphText = """
+A2 -- A1
+C2 -- C1
+D2 -- D1
+B2 -- B1
+H0 -- H1
+H1 -- H2
+H2 -- H3
+H3 -- H4
+H4 -- H5
+H5 -- H6
+H6 -- H7
+H7 -- H8
+H8 -- H9
+H9 -- HA
+A1 -- H2
+B1 -- H4
+C1 -- H6
+D1 -- H8
+"""
+
+def parse(text):
+    edges = [
+        tuple(map(lambda x: x.strip(),l.split("--")))
+        for l in text.strip().split("\n")
+    ]
+    return edges
+
+def build_graph(edges):
+    graph = defaultdict(lambda: {})
+    print(edges)
+    for v1, v2 in edges:
+        graph[v1][v2] = 1
+        graph[v2][v1] = 1
+    return graph
+
+
+def dijkstra(graph, st):
+    distances = {v: float('inf') for v in graph}
+    distances[st] = 0
+
+    pq = [(0, st)]
+    while len(pq) > 0:
+        d, current = heapq.heappop(pq)
+        if d > distances[current]:
+            continue
+        for neighbor, weight in graph[current].items():
+            distance = d + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+    return distances
+
+edges = parse(graphText)
+graph = build_graph(edges)
+print(graph)
+
+distance = {
+    (v1, v2): distance
+    for v1 in graph.keys()
+    for v2, distance in dijkstra(graph, v1).items()
+}
+
+print(distance[("A2", "C2")])
